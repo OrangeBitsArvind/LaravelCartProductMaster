76d8a50bf0a3307a38b0f271ec3167c20c5fee6a
Arvind <arvind.kapse@orangebitsindia.com>
2019-08-03 11:37:36 +0530

    New Changes

--- src/LaravelCartProductMasterServiceProvider.php
+++ src/LaravelCartProductMasterServiceProvider.php
@@ -17,6 +17,7 @@ class LaravelCartProductMasterServiceProvider extends ServiceProvider
         // $this->publishes([
         //     __DIR__.'/../database/migrations/2019_08_01_000100_create_products_table.php' => config_path('laravel_cart_product_master.php'),
         // ], 'laravel-cart-product-master');
+         
     }
 
     /**
